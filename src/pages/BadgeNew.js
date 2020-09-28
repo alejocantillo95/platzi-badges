10c10354b15f1f1605ab33b6e9a28e12c5668575
Alejandro Cantillo <jaimecantillo95@gmail.com>
2020-09-28 18:46:35 -0500

    Nuevo Proyecto

---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './styles/BadgeNew.css';
+import header from '../images/badge-header.svg';
+import Navbar from '../components/Navbar';
+import Badge from '../components/Badge';
+
+class BadgeNew extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="logo" />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge 
+                firstName="Alejandro" 
+                lastName="Cantillo" 
+                twitter="alejocantillo95" 
+                jobTitle="Full Stack JavaScript" 
+                avatarUrl="https://s.gravatar.com/avatar/be2cb070b9330f32915c816a50c9f5f0?s=80"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BadgeNew;
